05fddc7f12b86e6779045453f8f448e46bd8edaf
rtw369 <taewoorim@gmail.com>
2022-01-17 20:07:50 -0800

    Created function - turnIntoArray

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // sample formulas
 let formula1 = "a = 1 + 2";
 let formula2 = "a = b + c";
-let formula3 = "";
+let formula3 = "a+ b-c*d / e = (sinf+cosg)*Pi";
 
 let leftSide = "";
 let rightSide = "";
 
-divideFormula(removeSpaces(formula1));
+divideFormula(removeSpaces(formula3));
 console.log(leftSide);
+console.log(rightSide);
+
+rightSide = turnIntoArray(rightSide);
 console.log(rightSide);
 
 //when the program recieves the formula, take out the empty characters within a formula
@@ -25,5 +28,57 @@ function divideFormula(formula) {
 
 //add spaces in certain places and split by that spaces to create an array
 function turnIntoArray(expression) {
-    
+    let modifiedExpression = "";
+
+    for(let i = 0; i < expression.length; i++)
+    {
+        switch(expression.substr(i,3)) {
+            case "sin":
+            case "cos":
+            case "tan":
+                modifiedExpression += " "+expression.substr(i,3)+" ";
+                i += 3;
+            break;
+
+            default:
+        }
+
+        switch(expression.substr(i,2)) {
+            case "Pi":
+                modifiedExpression += " "+expression.substr(i,2)+" ";
+                i += 2;
+            break;
+
+            default:
+        }
+
+        switch(expression.substr(i,1)) {
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+            case "^":
+            case "(":
+            case ")":
+            case "E":
+                modifiedExpression += " "+expression.substr(i,1)+" ";
+            break;
+            
+            default:
+                modifiedExpression += expression.substr(i,1);
+                break;
+        }
+    }
+
+    let finalArray = modifiedExpression.split(" ");
+
+    // delete any empty string in the array
+    for(let i = 0; i < finalArray.length; i++)
+    {
+        if(finalArray[i] === "") {
+            finalArray.splice(i,1);
+        }
+    }
+
+    return finalArray;
 }
