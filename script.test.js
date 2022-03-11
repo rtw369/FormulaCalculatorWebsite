54b47012f276a6ada1cf0d00a9b17c4d6eb54e49
rtw369 <taewoorim@gmail.com>
2022-03-11 11:21:31 -0800

    Updated jest test

--- script.test.js
+++ script.test.js
@@ -1,5 +1,19 @@
 const script = require('./script');
 
-test('test formula: 2 + 6 / 3 - 4 = x', () => {
-    expect(script.execute("2 + 6 / 3 - 4 = x")).toBe(0);
+test('test formulae', () => {
+    let formula1 = "2+6/3-4 = x";
+    let formula2 = "((1+2)((3-4)(5+6))(7-8)) = x";
+
+    expect(script.execute(formula1)).toBe(0);
+    expect(script.execute(formula2)).toBe(33);
+});
+
+test('test quadratic functions', () => {
+    let quadratic1 = "x^2+2*x+1=0";
+    let quadratic2 = "(x+1)(x-1) = 0";
+    let quadratic3 = "(x+11)(x+4) = 0";
+
+    expect(script.execute(quadratic1)).toBe("-1\nor\n-1");
+    expect(script.execute(quadratic2)).toBe("1\nor\n-1");
+    expect(script.execute(quadratic3)).toBe("-4\nor\n-11");
 });
