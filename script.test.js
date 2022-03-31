6fbd08ae4940ad200934828fa335b7cf131e55f4
rtw369 <taewoorim@gmail.com>
2022-03-31 14:05:31 -0700

    Updated jest test

--- script.test.js
+++ script.test.js
@@ -5,9 +5,9 @@ test('test formulae', () => {
     let formula2 = "((1+2)((3-4)(5+6))(7-8)) = x";
     let formula3 = "((1+2)+((3-4)(5+6))-(7-8)) = x";
 
-    expect(script.execute(formula1)).toBe(0);
-    expect(script.execute(formula2)).toBe(33);
-    expect(script.execute(formula3)).toBe(-7);
+    expect(script.test(formula1)).toBe(0);
+    expect(script.test(formula2)).toBe(33);
+    expect(script.test(formula3)).toBe(-7);
 });
 
 test('test quadratic functions', () => {
@@ -15,9 +15,9 @@ test('test quadratic functions', () => {
     let quadratic2 = "(x+1)(x-1) = 0";
     let quadratic3 = "(x+11)(x+4) = 0";
 
-    expect(script.execute(quadratic1)).toBe("-1\nor\n-1");
-    expect(script.execute(quadratic2)).toBe("1\nor\n-1");
-    expect(script.execute(quadratic3)).toBe("-4\nor\n-11");
+    expect(script.test(quadratic1)).toBe("-1\nor\n-1");
+    expect(script.test(quadratic2)).toBe("1\nor\n-1");
+    expect(script.test(quadratic3)).toBe("-4\nor\n-11");
 });
 
 test('test more complicated equations', () => {
@@ -25,17 +25,17 @@ test('test more complicated equations', () => {
     formula2 = "(4-(x/3)) = 2";
     formula3 = "x+x+3 = x-7";
 
-    expect(script.execute(formula1)).toBe(0.5);
-    expect(script.execute(formula2)).toBe(6);
-    expect(script.execute(formula3)).toBe(-10);
+    expect(script.test(formula1)).toBe(0.5);
+    expect(script.test(formula2)).toBe(6);
+    expect(script.test(formula3)).toBe(-10);
 });
 
 test('test powers', () => {
     formula1 = "x = 2^3";
     formula2 = "(3)^2 = x"
 
-    expect(script.execute(formula1)).toBe(8);
-    expect(script.execute(formula2)).toBe(9);
+    expect(script.test(formula1)).toBe(8);
+    expect(script.test(formula2)).toBe(9);
 });
 
 test('test trignometry functions', () => {
@@ -46,12 +46,12 @@ test('test trignometry functions', () => {
     let acosine = "x = acos0.5";
     let atangent = "x = atan1";
 
-    expect(script.execute(sine)).toBe(1);
-    expect(script.execute(cosine)).toBe(0);
-    expect(Math.round(script.execute(tangent))).toBe(1);
-    expect(Math.round(script.execute(asine))).toBe(30);
-    expect(Math.round(script.execute(acosine))).toBe(60);
-    expect(Math.round(script.execute(atangent))).toBe(45);
+    expect(script.test(sine)).toBe(1);
+    expect(script.test(cosine)).toBe(0);
+    expect(Math.round(script.test(tangent))).toBe(1);
+    expect(Math.round(script.test(asine))).toBe(30);
+    expect(Math.round(script.test(acosine))).toBe(60);
+    expect(Math.round(script.test(atangent))).toBe(45);
 });
 
 test('a little more complicated trignometry functions', () => {
@@ -60,8 +60,8 @@ test('a little more complicated trignometry functions', () => {
     let function3 = "30*sinx = 30";
     let function4 = "30*sin(x+3) = 30";
 
-    expect(script.execute(function1)).toBe(-2);
-    expect(script.execute(function2)).toBe(1);
-    expect(script.execute(function3)).toBe(90);
-    expect(script.execute(function4)).toBe(87);
+    expect(script.test(function1)).toBe(-2);
+    expect(script.test(function2)).toBe(1);
+    expect(script.test(function3)).toBe(90);
+    expect(script.test(function4)).toBe(87);
 });
